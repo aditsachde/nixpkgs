ae394df8973efa81aaa1c9c677de38c8d2696477
Adit Sachde <23707194+aditsachde@users.noreply.github.com>
2020-06-01 23:09:55 -0400

    plex: 1.19.2.2737 -> 1.19.4.2893

--- pkgs/servers/plex/raw.nix
+++ pkgs/servers/plex/raw.nix
@@ -8,13 +8,13 @@
 # server, and the FHS userenv and corresponding NixOS module should
 # automatically pick up the changes.
 stdenv.mkDerivation rec {
-  version = "1.19.2.2737-b69929dab";
+  version = "1.19.4.2893-1d4670196";
   pname = "plexmediaserver";
 
   # Fetch the source
   src = fetchurl {
     url = "https://downloads.plex.tv/plex-media-server-new/${version}/redhat/plexmediaserver-${version}.x86_64.rpm";
-    sha256 = "00s3ab66wnpwkjrp0ng8srcy3l32sh5ibv0i9m6l4d0a19hr01fs";
+    sha256 = "03030y6fc0q44326qjkzghnh3nxzdimwkb0wgfiyv59cwa4c5j3j";
   };
 
   outputs = [ "out" "basedb" ];
